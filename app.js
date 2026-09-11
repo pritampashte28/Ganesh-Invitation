e74c0d45abbcd7fd94d519f94ccf7121282dc61d
pritampashte28 <pritampashte28@gmail.com>
2026-09-11 16:54:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@
 
   /* --- Configuration -------------------------------------- */
   var CONFIG = {
-    mapsUrl: 'https://maps.app.goo.gl/cRY1QJZiN5nMUKr87',
+    mapsUrl: 'https://maps.app.goo.gl/Az7tcAVevXDjv9Lf8',
     showWhatsApp: true,
     showPetals: true,
     shareMessage: 'Ganpati Bappa Morya! You and your family are invited for darshan ' +
-                  'and aarti at our home on 14 & 15 September 2026. Directions: '
+                  'and aarti at our home on 14 till 18 September 2026. Directions: '
   };
 
   /* --- Element lookup ------------------------------------- */
